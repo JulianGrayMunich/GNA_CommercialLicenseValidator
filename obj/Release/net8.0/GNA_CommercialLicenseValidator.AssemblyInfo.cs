90b714da7c87b62a11e30ae3ca9ec275e171b38d
gna.geomatics <gna.geomatics@gmail.com>
2026-02-26 14:51:00 +0000

    All the historic data and deltas are computed and uploaded to all the worksheets. Tested and working. Milestone.

--- obj/Release/net8.0/GNA_CommercialLicenseValidator.AssemblyInfo.cs
+++ obj/Release/net8.0/GNA_CommercialLicenseValidator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GNA_CommercialLicenseValidator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deb6e6a55406491644f5154d4c58acaeec49f2e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ba9fd602b4984d929ff08a0af22845bcab88824")]
 [assembly: System.Reflection.AssemblyProductAttribute("GNA_CommercialLicenseValidator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GNA_CommercialLicenseValidator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
