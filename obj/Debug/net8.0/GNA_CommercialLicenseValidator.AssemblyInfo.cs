367853d47974ef9cbe31adec4c9a0bbf8ec6e42e
gna.geomatics <gnajag2025@gmail.com>
2026-04-14 15:18:06 +0100

    Project progressing. All working at this point.

--- obj/Debug/net8.0/GNA_CommercialLicenseValidator.AssemblyInfo.cs
+++ obj/Debug/net8.0/GNA_CommercialLicenseValidator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GNA_CommercialLicenseValidator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b5f600876229bacba1186a07f95682adc85f7b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e620be964f4207696f4193f6a3c8bc312dd67252")]
 [assembly: System.Reflection.AssemblyProductAttribute("GNA_CommercialLicenseValidator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GNA_CommercialLicenseValidator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
