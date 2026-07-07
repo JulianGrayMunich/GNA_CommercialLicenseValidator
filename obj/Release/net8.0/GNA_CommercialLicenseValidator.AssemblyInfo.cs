a431ba47163b5945c4f958b9eeeec6b6bb422ca4
gna.geomatics <gnajag2025@gmail.com>
2026-07-07 17:49:32 +0200

    Historic Top and dH added

--- obj/Release/net8.0/GNA_CommercialLicenseValidator.AssemblyInfo.cs
+++ obj/Release/net8.0/GNA_CommercialLicenseValidator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GNA_CommercialLicenseValidator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+367853d47974ef9cbe31adec4c9a0bbf8ec6e42e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bff4a2e329da1885ddeaefb0adfde5388b38db61")]
 [assembly: System.Reflection.AssemblyProductAttribute("GNA_CommercialLicenseValidator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GNA_CommercialLicenseValidator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
